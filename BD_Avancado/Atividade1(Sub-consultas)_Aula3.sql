cff9267bd6b776eee55042c4df2a074b229ac0a6
Mateus Dassoler Spezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2026-03-09 17:02:56 -0300

    Finalizando atividade

--- BD_Avancado/Atividade1(Sub-consultas)_Aula3.sql
+++ BD_Avancado/Atividade1(Sub-consultas)_Aula3.sql
@@ -18,16 +18,66 @@ HAVING COUNT(*) >= ALL (SELECT COUNT(*) qtde
 								GROUP BY vc.nr_placa);
 
 -- a) Recuperar o(s) acessório(s) que não estão associado(s) a nenhum veículo
+SELECT a.ds_acessorio
+FROM acessorio a
+WHERE a.cd_acessorio NOT IN (SELECT va.cd_acessorio
+										FROM veiculo_acessorio va)
+GROUP BY a.ds_acessorio;
+
+-- b) Listar a descrição do(s) acessório(s) mais popular (que mais é encontrado entre os veículos cadastrados);
 SELECT a.ds_acessorio, COUNT(*) AS qtde
 FROM acessorio a, veiculo v, veiculo_acessorio va
 WHERE a.cd_acessorio = va.cd_acessorio
 	AND va.nr_placa = v.nr_placa
-GROUP BY a.ds_acessorio;
-
--- b) Listar a descrição do(s) acessório(s) mais popular (que mais é encontrado entre os veículos cadastrados);
+GROUP BY a.ds_acessorio
+HAVING COUNT(*) >= ALL (SELECT COUNT(*) 
+								FROM veiculo_acessorio va
+								GROUP BY va.cd_acessorio);
 
 -- c) Listar a descrição da marca que possui o maior número de veículos cadastrado;
+SELECT m.ds_marca, COUNT(*) AS qtde
+FROM veiculo v, modelo mo, marca m
+WHERE v.cd_modelo = mo.cd_modelo
+	AND m.cd_marca = mo.cd_marca
+GROUP by m.ds_marca
+HAVING COUNT(*) >= ALL (SELECT COUNT(*) qtde
+								FROM veiculo v, modelo mo
+								WHERE v.cd_modelo = mo.cd_modelo
+								GROUP BY mo.cd_marca);
 
 -- d) Listar a descrição do combustível que apresenta o menor número de veículos associado;
+SELECT c.ds_combustivel, COUNT(*) AS qtde
+FROM veiculo v, combustivel c, veiculo_combustivel vc
+WHERE v.nr_placa = vc.nr_placa
+	AND c.cd_combustivel = vc.cd_combustivel
+GROUP BY c.ds_combustivel
+HAVING COUNT(*) <= ALL (SELECT COUNT(*) qtde
+								FROM veiculo_combustivel vc
+								GROUP BY vc.cd_combustivel);
+								
+-- e) Listar a(s) marca(s) e modelo(s) de veículo(s) movido(s) ao combustível "Elétrico" e que apresenta(m) acessório "Volante multifuncional".
+SELECT m.ds_marca, mo.ds_modelo
+FROM veiculo v, modelo mo, marca m
+WHERE v.cd_modelo = mo.cd_modelo
+	AND mo.cd_marca = m.cd_marca
+	AND v.nr_placa IN (
+    SELECT vc.nr_placa
+    FROM veiculo_combustivel vc
+    WHERE vc.cd_combustivel = (
+        SELECT c.cd_combustivel
+        FROM combustivel c
+        WHERE c.ds_combustivel = 'Elétrico'
+    )
+)
+	AND v.nr_placa IN (
+    SELECT va.nr_placa
+    FROM veiculo_acessorio va
+    WHERE va.cd_acessorio = (
+        SELECT a.cd_acessorio
+        FROM acessorio a
+        WHERE a.ds_acessorio = 'Volante multifuncional'
+    )
+)
+GROUP BY m.ds_marca, mo.ds_modelo
+ORDER BY m.ds_marca;
 
--- e) Listar a(s) marca(s) e modelo(s) de veículo(s) movido(s) ao combustível "Elétrico" e que apresenta(m) acessório "ADAS".
